618c51c6605df7c73293e01d427a3bd3bb9e27ac
Gary Huang <gary_1327@yahoo.com>
2022-11-16 15:59:12 -0500

    solutions for 8.2 and 8.3

--- primer/8.1.cpp
+++ primer/8.1.cpp
@@ -2,13 +2,14 @@
 #include <string>
 
 std::istream& readAndPrint(std::istream& is) {
-	std::string a;
+	std::string a; 
 	while (is >> a) {
 		std::cout << a << ' ' << std::endl;
 	}
 	std::cout << std::endl;
 
 	is.clear();
+
 	return is;
 }
 
