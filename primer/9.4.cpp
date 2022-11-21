d9d2c2065bf1abd9a7bbb0134dc6a837f50ab177
Gary Huang <gary_1327@yahoo.com>
2022-11-20 21:33:41 -0500

    solution for 9.4

---
+++ primer/9.4.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+
+bool hasVal(std::vector<int>::const_iterator begin, std::vector<int>::const_iterator end, int val) {
+	while (begin != end) {
+		if (*begin == val) {
+			return true; 
+		}
+		++begin; 
+	}
+	return false; 
+}
+
+int main() {
+	std::vector<int> v; 
+	for (int i = 0; i <= 10; ++i) {
+		v.push_back(i); 
+	}
+
+	int val; 
+	std::cin >> val; 
+
+	std::cout << hasVal(v.begin(), v.end(), val) << std::endl; 
+
+	return 0; 
+}
