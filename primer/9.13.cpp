90afe74deb804931120552f8dd853d2d8dfe3297
Gary Huang <gary_1327@yahoo.com>
2022-11-22 18:03:01 -0500

    solution for 9.13

---
+++ primer/9.13.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <list>
+#include <iostream>
+
+int main() {
+	std::list<int> l1 = { 1, 2, 3 };
+	std::vector<int> v1 = { 3, 2, 1 };
+
+	std::vector<double> vl1(l1.begin(), l1.end()); 
+	std::vector<double> vv1(v1.begin(), v1.end()); 
+
+	for (auto i : vl1) {
+		std::cout << i << ' '; 
+	}
+	std::cout << std::endl; 
+
+	for (auto i : vv1) {
+		std::cout << i << ' ';
+	}
+	std::cout << std::endl; 
+
+	return 0; 
+}
