51f32a40fd343c8ed769614bed06773e374d9372
Gary Huang <gary_1327@yahoo.com>
2022-11-24 10:13:43 -0500

    solution for 9.18

--- primer/9.16.cpp
+++ primer/9.16.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <sstream>
 #include <list>
-
+/*
 int main() {
 	std::list<int> l; 
 	std::vector<int> v; 
@@ -60,3 +60,4 @@ int main() {
 
 	return 0; 
 }
+*/
---
+++ primer/9.18.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <deque>
+#include <iostream>
+
+void read(std::deque<std::string>& ds) {
+	std::string s; 
+	while (std::cin >> s) {
+		ds.push_back(s); 
+	}
+	std::cin.clear(); 
+	return; 
+}
+
+void print(const std::deque<std::string>& ds) {
+	for (auto it = ds.cbegin(); it != ds.cend(); ++it) {
+		std::cout << *it << '\n';
+	}
+	std::cout << std::flush; 
+	return; 
+}
+
+int main() {
+	std::deque<std::string> ds; 
+	read(ds); 
+	print(ds);
+
+	return 0; 
+}
