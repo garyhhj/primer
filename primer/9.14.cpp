58d70349dc3704b6539ea0994a2a73df375a53fe
Gary Huang <gary_1327@yahoo.com>
2022-11-23 19:33:14 -0500

    solution for 9.15

--- primer/9.14.cpp
+++ primer/9.14.cpp
@@ -2,10 +2,11 @@
 #include <list>
 #include <string>
 #include <iostream>
-
+/*
 int main() {
 	std::list<char*> lc{ 'a' }; 
 	std::vector<std::string> vs; 
 	vs.assign(lc.begin(), lc.end()); 
 	
 }
+*/
---
+++ primer/9.15.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include <iostream>
+int main() {
+	std::vector<int> v1; 
+	std::vector<int> v2 = { 1, 2 };
+
+	std::cout << (v1 == v2) << std::endl; 
+}
