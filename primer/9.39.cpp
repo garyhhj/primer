5309414c23a5a201324be769e2587af00f489188
Gary Huang <gary_1327@yahoo.com>
2022-12-01 11:55:07 -0500

    solution for 9.39

---
+++ primer/9.39.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::vector<std::string> svec; 
+	svec.reserve(1024); //preallocates 1024 'strings'
+
+	std::string word;
+	while (std::cin >> word) {
+		svec.push_back(word); 
+	}
+
+	//it will insert svec.size()/2 amount of value intialized elements to svec
+	//it may or may not need to reallcoate memory depending on the amount of elements inserted 
+	svec.resize(svec.size() + svec.size() / 2); 
+}
