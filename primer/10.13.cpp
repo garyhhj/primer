dc2b102bd1f837206aee5720c01fb6796f82933c
Gary Huang <gary_1327@yahoo.com>
2022-12-21 15:43:42 -0500

    solution for 10.13

---
+++ primer/10.13.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+/*
+bool lessThanFive(const std::string& s) {
+	return s.size() >= 5; 
+}
+
+void print(const std::vector<std::string>& vs) {
+	for (auto s : vs) {
+		std::cout << s << ' ';
+	}
+	std::cout << std::endl; 
+}
+
+int main() {
+	std::vector<std::string> vs; 
+	std::string s; 
+
+	while (std::cin >> s) {
+		vs.push_back(s); 
+	}
+
+	std::partition(vs.begin(), vs.end(), lessThanFive); 
+	print(vs); 
+
+}
+*/
