e99458109558afbbf2e185e5d0998259a38c53cc
Gary Huang <gary_1327@yahoo.com>
2022-12-21 15:37:07 -0500

    solution for 10.12

---
+++ primer/10.12.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+/*
+//will sort string since sales data is just string 
+bool compare(const std::string& s1, const std::string& s2) {
+	return s1 < s2; 
+}
+
+void print(const std::vector<std::string> vs) {
+	for (const auto s : vs) {
+		std::cout << s << ' '; 
+	}
+	std::cout << std::endl; 
+}
+
+int main(){
+	std::vector<std::string> words; 
+	std::string word; 
+	while (std::cin >> word) {
+		words.push_back(word);
+	}
+
+	std::sort(words.begin(), words.end(), compare); 
+
+	print(words); 
+}
+*/
