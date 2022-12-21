4afcfa6939e4b5021e53fa533890f28cc5db8d22
Gary Huang <gary_1327@yahoo.com>
2022-12-20 23:09:01 -0500

    solution for 10.7

---
+++ primer/10.7.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <list>
+
+int main() {
+	
+	//part a 
+	std::vector<int> vec; 
+	std::list<int> lst; 
+	int i; 
+	while (std::cin >> i) 
+		lst.push_back(i); 
+	//copy(lst.cbegin(), lst.cend(), vec.cbegin())
+	copy(lst.cbegin(), lst.cend(), std::back_inserter(vec)); 
+
+	for (auto i : vec) {
+		std::cout << i << ' ';
+	}
+	std::cout << std::endl;
+	
+	//clearing vec and lst  
+	vec.clear(); 
+	lst.clear(); 
+	
+
+	//part b
+	//vec.reserve(10); 
+	vec.resize(10); 
+	std::fill_n(vec.begin(), 10, 0); 
+
+
+	for (auto i : vec) {
+		std::cout << i << ' '; 
+	}
+	std::cout << std::endl; 
+
+}
