69618b92cccfde6930bb197bbe4b97ac90628e07
Gary Huang <gary_1327@yahoo.com>
2023-01-22 18:34:48 -0500

    solution for 10.16

---
+++ primer/10.16.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+//prints number of words of a given size or greater
+//prints the words of that size or greaqter 
+void biggies(std::vector<std::string>& words, std::vector<std::string>::size_type sz) {
+
+	
+	int count = 0; 
+	for (auto& word : words) {
+		auto greater = [word, sz]()->bool {return word.size() >= sz; };
+		if (greater()) {
+			std::cout << word << '\n';
+			++count; 
+		}
+	}
+	std::cout << "number of words: " << count << '\n'; 
+}
+
+int main() {
+	std::vector<std::string> words; 
+	std::string word; 
+	while (std::cin >> word) {
+		words.push_back(word); 
+	}
+
+	std::cin.clear(); 
+
+	int sz; 
+	std::cin >> sz; 
+
+	biggies(words, sz); 
+}
