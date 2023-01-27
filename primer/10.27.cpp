99461b31f65a614ac9f3081e33e988f47f203ba6
Gary Huang <gary_1327@yahoo.com>
2023-01-27 05:48:52 -0500

    solution for 10.28

--- primer/10.27.cpp
+++ primer/10.27.cpp
@@ -3,16 +3,15 @@
 #include <algorithm>
 #include <iostream>
 
+/*
 int main() {
-	std::vector<int> vi; 
-	std::list<int> li; 
-
-	int i; 
-	while (std::cin >> i) {
-		vi.push_back(i); 
-	}
+	std::vector<int> vi = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	std::list<int> libi;
 	
-	std::unique_copy(vi.begin(), vi.end(), std::back_inserter(li)); 
+	std::unique_copy(vi.begin(), vi.end(), std::back_inserter(libi)); 
 
-	for (auto i : li) std::cout << i << " "; 
+	for (auto i : libi) std::cout << i << " ";
+	std::cout << "\n";
+
 }
+*/
---
+++ primer/10.28.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <iostream>
+
+int main() {
+	std::vector<int> vi = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	std::list<int> lifi;
+	std::list<int> libi;
+	std::list<int> lii;
+
+
+
+	std::unique_copy(vi.begin(), vi.end(), std::back_inserter(libi));
+	std::unique_copy(vi.begin(), vi.end(), std::front_inserter(lifi));
+	std::unique_copy(vi.begin(), vi.end(), std::inserter(lii, lii.begin()));
+
+	for (auto i : lifi) std::cout << i << " ";
+	std::cout << "\n";
+
+
+	for (auto i : libi) std::cout << i << " ";
+	std::cout << "\n";
+
+
+	for (auto i : lii) std::cout << i << " ";
+	std::cout << "\n";
+}
