4e2172f7803d6b74175fcb2c68bf5a9f26855c84
Gary Huang <gary_1327@yahoo.com>
2023-01-28 15:21:42 -0500

    solution for 10.38

--- primer/10.37.cpp
+++ primer/10.37.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
-
+/*
 int main() {
 	std::vector<int> vi = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; 
 
@@ -12,3 +12,4 @@ int main() {
 
 	for (auto i : li) std::cout << i << " "; 
 }
+*/
