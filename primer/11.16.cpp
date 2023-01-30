59d4fba9049547dc1f0eeb31753f1c7716d895eb
Gary Huang <gary_1327@yahoo.com>
2023-01-30 15:24:02 -0500

    solution for 11.16

---
+++ primer/11.16.cpp
@@ -0,0 +1,14 @@
+#include <map>
+#include <iostream>
+
+
+int main() {
+	std::map<int, int> mp = { {1, 2} }; 
+
+	std::map<int, int>::iterator mp_it = mp.begin(); 
+	if (mp_it != mp.end()) {
+		mp_it->second = 0; 
+	}
+
+	std::cout << mp_it->first << " : " << mp_it->second; 
+}
