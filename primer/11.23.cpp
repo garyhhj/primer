9a0cefb89ef2f94666b1f56c370cb4c98f0c996a
Gary Huang <gary_1327@yahoo.com>
2023-01-30 20:22:50 -0500

    solution for 11.23

---
+++ primer/11.23.cpp
@@ -0,0 +1,30 @@
+#include <map>
+#include <iostream>
+#include <string>
+
+/*
+void print(std::multimap<std::string, std::string>& lname_fname) {
+	
+	std::cout << "printing map: \n";
+	for (auto& i : lname_fname) {
+		std::cout << i.first << " " << i.second << "\n"; 
+	}
+
+}
+
+void read(std::multimap<std::string, std::string>& lname_fname) {
+	
+	std::string lname, fname; 
+	while (std::cin >> lname >> fname) {
+		lname_fname.insert({ lname, fname }); 
+	}
+}
+
+int main() {
+	std::multimap<std::string, std::string> lname_fname; 
+	read(lname_fname); 
+	print(lname_fname); 
+
+
+}
+*/
