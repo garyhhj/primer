66459837451e3cd82b5bb0b8a51a75db037defff
Gary Huang <gary_1327@yahoo.com>
2023-01-31 21:27:06 -0500

    solution for 11.28

---
+++ primer/11.28.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+int main() {
+	std::map<std::string, std::vector<int>> str_vi; 
+
+	std::map<std::string, std::vector<int>>::iterator
+		it = str_vi.find("meow"); 
+
+}
