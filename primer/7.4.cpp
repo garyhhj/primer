94c99f59de2cd9954a559eacef65303acccae1e5
Gary Huang <gary_1327@yahoo.com>
2023-02-06 17:01:55 -0500

    solution for 7.5

--- primer/7.4.cpp
+++ primer/7.4.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 
+/*
 class Person {
 private: 
 	std::string name; 
 	std::string address; 
 };
+*/
---
+++ primer/7.5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+/*
+class Person {
+public: 
+
+	//functions should be const since we aren't changing the name or address 
+	std::string getName() const { return name; }
+	std::string getAddress() const { return address; }
+
+private:
+	std::string name;
+	std::string address;
+};
+*/
