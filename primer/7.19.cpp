ce37617f080608866acd41ef91c63b7ad18d65da
Gary Huang <gary_1327@yahoo.com>
2023-02-07 14:49:32 -0500

    solution for 7.18 and 7.19

---
+++ primer/7.19.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+//data and implmentation are kept private 
+//interface is kept public 
+
+class Person {
+public:
+	Person() = default;
+	Person(const std::string& initialName, const std::string& initialAddress) : name(initialName), address(initialAddress) {}
+
+	std::string getName() const { return name; }
+	std::string getAddress() const { return address; }
+
+private:
+	std::string name;
+	std::string address;
+};
