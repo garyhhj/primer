23c23080589bf011b441e19c0df253cc39228713
Gary Huang <gary_1327@yahoo.com>
2023-02-07 14:25:36 -0500

    solution for 7.15

---
+++ primer/7.15.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+/*
+class Person {
+public:
+	Person() = default;
+	Person(const std::string& initialName, const std::string& initialAddress) : name(initialName), address(initialAddress) {}
+
+	//functions should be const since we aren't changing the name or address
+	std::string getName() const { return name; }
+	std::string getAddress() const { return address; }
+
+private:
+	std::string name;
+	std::string address;
+};
+
+int main() {
+	Person p1; 
+	std::cout << p1.getName() << " " << p1.getAddress() << std::endl; 
+
+	Person p2("cat", "catland"); 
+	std::cout << p2.getName() << " " << p2.getAddress() << std::endl;
+
+}
+*/
