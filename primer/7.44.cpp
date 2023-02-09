7a498bd33abbaf6161a9a50aaf7107c47e844a22
Gary Huang <gary_1327@yahoo.com>
2023-02-09 14:18:26 -0500

    solution for 7.44

---
+++ primer/7.44.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+/*
+class NoDefault {
+public:
+	NoDefault(int n) : a(n) {
+		std::cout << "constructor for class NoDefault" << std::endl;
+	}
+private:
+	int a;
+};
+
+class C {
+public:
+	C() : nd(0) {
+		std::cout << "constructor for class C" << std::endl;
+	}
+private:
+	NoDefault nd;
+};
+
+
+int main() {
+	//not legal, we did not define a default constructor for class NoDefault 
+	std::vector<NoDefault>vec(10); 
+
+}
+*/
