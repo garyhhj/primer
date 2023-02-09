90ca6efa9f932fcd8e5395d8819aaaf8335b5d7d
Gary Huang <gary_1327@yahoo.com>
2023-02-08 22:40:23 -0500

    solution for 7.40

---
+++ primer/7.40.cpp
@@ -0,0 +1,26 @@
+//understanding order of delegating constructor 
+#include <iostream>
+
+class Meow {
+public: 
+	Meow() : a(1) {
+		std::cout << a << std::endl; 
+	}
+
+	Meow(int na) : Meow() {
+		a = 2; 
+		std::cout << a << std::endl; 
+
+	}
+
+	Meow(int na, int nb) : Meow(3) {
+		a = 3; 
+		std::cout << a << std::endl;
+	}
+	
+	int a; 
+};
+
+int main() {
+	Meow meow(2, 3); 
+}
