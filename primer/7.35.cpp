1e5a8a4864afe1b2e60c9b1e50f2ff27302942f9
Gary Huang <gary_1327@yahoo.com>
2023-02-08 19:05:34 -0500

    solution for 7.35

---
+++ primer/7.35.cpp
@@ -0,0 +1,21 @@
+#include <string>
+/*
+using namespace std; 
+
+typedef char Type;
+Type initVal();
+class Exercise {
+public:
+	typedef double Type;
+	Type setVal(Type); //double setVal(double)
+	Type initVal(); //double initVal()
+private:
+	int val;
+};
+
+//Type Exercise::setVal(Type parm) { // char Exercise::setVal(double parm)
+double Exercise::setVal(double parm){
+	val = parm + initVal(); return val;
+}
+*/
+
