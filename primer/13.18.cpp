774fb333134e4c6aa7c5dd54d84b03854e19d921
Gary Huang <gary_1327@yahoo.com>
2023-02-13 14:00:30 -0500

    solution for 13.18

---
+++ primer/13.18.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+class Employee {
+public: 
+
+	Employee() : name(std::string()), id(++uid) {}
+	Employee(const std::string newName) : name(newName), id(++uid) {}
+
+	std::string name; 
+	int id; 
+
+private: 
+	static int initUid(); 
+	static int uid; 
+};
+
+int Employee::initUid() { return 1; }
+int Employee::uid = Employee::initUid(); 
+
+int main() {
+	Employee g; 
+	std::cout << g.name << " " << g.id << std::endl; 
+}
