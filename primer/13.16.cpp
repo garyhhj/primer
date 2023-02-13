df0e159e13c12cc78cd2c6db8be09c3fc0d64df1
Gary Huang <gary_1327@yahoo.com>
2023-02-13 13:49:27 -0500

    solution for 13.16

---
+++ primer/13.16.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+using std::cout; using std::endl;
+
+class numbered {
+public:
+	numbered() : mysn(++sn) {}
+	int mysn;
+
+	numbered(const numbered& rhs) :mysn(++sn) {
+	}
+
+private:
+	static int sn;
+};
+
+int numbered::sn = 0;
+
+void f(const numbered& s) {
+	cout << s.mysn << endl;
+}
+
+int main() {
+	numbered a, b = a, c = b;
+	f(a);  // 1
+	f(b);  // 2
+	f(c);  // 3
+	numbered d;
+	f(d);  // 4
+
+	return 0;
+}
