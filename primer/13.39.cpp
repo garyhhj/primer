eadfd20847f0c6289f4feb3f7958faed002f4e61
Gary Huang <gary_1327@yahoo.com>
2023-02-14 16:25:48 -0500

    solution for 13.45

---
+++ primer/13.39.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*
+class StrVec {
+
+	//todo: write the class, I am a bit lazy to write a dynamic array of strings 
+private: 
+	std::string* firstElement; 
+	std::string* lastELement; 
+	std::string* cap; 
+};
+*/
