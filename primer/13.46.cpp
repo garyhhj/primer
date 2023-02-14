7dc7a07850f67894b3c1631955cbfccd27ea334a
Gary Huang <gary_1327@yahoo.com>
2023-02-14 18:39:53 -0500

    solution for 13.46

---
+++ primer/13.46.cpp
@@ -0,0 +1,16 @@
+
+
+#include<iostream>
+#include <vector>
+/*
+int f() { return 1; }
+std::vector<int> vi(100);
+
+
+int main() {
+	int r1 = f(); 
+	int& r2 = vi[0]; 
+	int& r3 = r1; 
+	int r4 = vi[0] * f();
+}
+*/
