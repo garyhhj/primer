b9bd41f3480132b8f88d536bb55f9ee59464a8b5
Gary Huang <gary_1327@yahoo.com>
2023-02-17 15:50:03 -0500

    solution for 14.42

---
+++ primer/14.42.cpp
@@ -0,0 +1,20 @@
+#include <functional>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main() {
+
+
+	std::greater<int> a; 
+	int count = 0; 
+	std::vector<int> vi = { 1, 1, 1, 1024, 1024, 2000, 2000};
+	for (const auto i : vi) {
+		if (a(i, 1024)) ++count; 
+	}
+	std::cout << "greater than 1024: "; 
+	std::cout << count << std::endl; 
+
+
+}
