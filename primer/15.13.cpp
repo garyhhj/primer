6171e374cf092d25777c9c168d037ba4798db414
Gary Huang <gary_1327@yahoo.com>
2023-02-19 15:01:22 -0500

    solution for 15.13

---
+++ primer/15.13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+class base{
+public:
+ string name() { return basename; } virtual void print(ostream & os) { os << basename; }
+private:
+ string basename;
+};
+class derived : public base {
+public:
+	void print(ostream& os) { base::print(os); os << " " << i; } //change print(os) to base::print(os) to avoid recursive call
+private:
+	int i;
+};
+
+int main() {
+	derived d; 
+	d.print(std::cout); 
+}
