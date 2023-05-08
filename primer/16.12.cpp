f81e98c424124d7803a8e5d114cd4fc4b37f95b0
Gary Huang <gary_1327@yahoo.com>
2023-05-08 17:43:03 -0400

    blob class for 16.12

---
+++ primer/16.12.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+template <typename T> 
+class blob {
+	typename typedef std::vector<T>::size_type size_type;
+
+public: 
+	void push_back(const T& val);
+	void pop_back(); 
+
+	T& operator[](size_type size);
+
+private: 
+	std::vector<T> data; 
+
+};
+
+template <typename T> 
+void blob<T>::push_back(const T& val) {
+	blob<T>::data.push_back(val); 
+}
+
+template <typename T> 
+void blob<T>::pop_back() {
+	blob<T>::data.pop_back(); 
+}
+
+template <typename T> 
+T& blob<T>::operator[](size_type size) {
+	return blob<T>::data[size]; 
+}
+
+
+int main() {
+	blob<int> b; 
+	b.push_back(1); 
+	std::cout << b[0] << std::endl; 
+}
