1770b0d56f6fead79cd9dc876fe1ead4b5bad088
Gary Huang <gary_1327@yahoo.com>
2023-05-09 20:11:52 -0400

    solution for 16.19

---
+++ primer/16.19.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+
+template <typename T> 
+void print(const T& container) {
+	typedef typename T::size_type n; 
+
+	//what is the universal way to get something idk 
+
+	std::cout << "conatiner: "; 
+	for (n i = 0; i < container.size(); ++i) {
+		std::cout << container[i] << ", "; 
+	}
+	std::cout << std::endl; 
+}
+
+int main() {
+
+	std::vector<int> meow = { 1, 2, 3, 4 }; 
+	print(meow); 
+}
